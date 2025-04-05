4158dfb7ffbf236e2b7eee2d1901c2d78dc3ffa6
Asif Ali <asifalieas@gmail.com>
2025-04-05 17:39:49 +0530

    Two way binding : Set name on click

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,32 @@
+import { useState } from 'react'
+
 export default function Player() {
+  const [enteredPlayerName, setEnteredPlayerName] = useState('')
+  const [submitted, setSubmitted] = useState(false)
+
+  function handleNameChange(e) {
+    setSubmitted(false)
+    setEnteredPlayerName(e.target.value)
+  }
+
+  function handleSubmitClick() {
+    setSubmitted(true)
+  }
+
+  console.log('entered Name : ', enteredPlayerName)
+  console.log('submited flag : ', submitted)
+
   return (
-    <section id="player">
-      <h2>Welcome unknown entity</h2>
+    <section id='player'>
+      <h2>Welcome {submitted ? enteredPlayerName : 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input
+          onChange={handleNameChange}
+          type='text'
+          value={enteredPlayerName}
+        />
+        <button onClick={handleSubmitClick}>Set Name</button>
       </p>
     </section>
-  );
+  )
 }
